b59676ca17e70dce63e727356f588fd1f4aa3181
Spraypax <114649460+Spraypax@users.noreply.github.com>
2025-12-09 23:33:10 +0100

    lib.rs + boot.rs (WIP)

---
+++ src/boot.rs
@@ -0,0 +1,35 @@
+pub struct BootSector {
+    pub bytes_per_sector: u16,
+    pub sectors_per_cluster: u8,
+    pub reserved_sectors: u16,
+    pub num_fats: u8,
+    pub sectors_per_fat: u32,
+    pub root_cluster: u32,
+    
+}
+
+impl BootSector {
+    pub fn parse(sector: &[u8]) -> Result<Self, Error> {
+        if sector.len() < 512 {
+            return Err(Error::InvalidFs);
+        }
+
+        let bytes_per_sector = u16::from_le_bytes([sector[11], sector[12]]);
+        let sectors_per_cluster = sector[13];
+        let reserved_sectors = u16::from_le_bytes([sector[14], sector[15]]);
+        let num_fats = sector[16];
+        let sectors_per_fat = u32::from_le_bytes([sector[36], sector[37], sector[38], sector[39]]);
+        let root_cluster = u32::from_le_bytes([sector[44], sector[45], sector[46], sector[47]]);
+
+        // Checks
+
+        Ok(Self {
+            bytes_per_sector,
+            sectors_per_cluster,
+            reserved_sectors,
+            num_fats,
+            sectors_per_fat,
+            root_cluster,
+        })
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,13 @@
 #[cfg(not(feature = "std"))]
 extern crate alloc;
 
+// --- tes modules internes ---
 pub mod boot;
 pub mod fat;
 pub mod dir;
 pub mod file;
 
-// un trait pour l’accès disque
+// --- Ton trait BlockDevice ---
 pub trait BlockDevice {
     fn read_sector(&mut self, lba: u64, buf: &mut [u8]) -> Result<(), Error>;
 }
@@ -17,10 +18,41 @@ pub trait BlockDevice {
 pub enum Error {
     Io,
     InvalidFs,
+    
 }
 
-// façade principale
-pub struct Fat32<D: BlockDevice> {
-    device: D,
-    // boot info, offsets...
+#[cfg(feature = "std")]
+pub mod std_support {
+    use std::fs::File;
+    use std::io::{Read, Seek, SeekFrom};
+
+    use crate::{BlockDevice, Error};
+
+    pub struct StdBlockDevice {
+        file: File,
+        pub sector_size: u64,
+    }
+
+    impl StdBlockDevice {
+        pub fn open(path: &str, sector_size: u64) -> std::io::Result<Self> {
+            let file = File::open(path)?;
+            Ok(Self { file, sector_size })
+        }
+    }
+
+    impl BlockDevice for StdBlockDevice {
+        fn read_sector(&mut self, lba: u64, buf: &mut [u8]) -> Result<(), Error> {
+            let offset = lba * self.sector_size;
+
+            self.file
+                .seek(SeekFrom::Start(offset))
+                .map_err(|_| Error::Io)?;
+
+            self.file
+                .read_exact(buf)
+                .map_err(|_| Error::Io)?;
+
+            Ok(())
+        }
+    }
 }
